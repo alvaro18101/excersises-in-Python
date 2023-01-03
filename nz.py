f67cc086362910a23667fc3c5021f19fbf7a667d
Alvaro Alejandro Siesquen Abad <75409414+alvaro18101@users.noreply.github.com>
2023-01-02 22:56:39 -0500

    Add files via upload

--- nz.py
+++ nz.py
@@ -10,4 +10,13 @@ def multiply(a,b):
 print(multiply(5,-3))
 
 
-# 
+# PROBLEMA 2
+import functools
+
+def maximum(array):
+    return functools.reduce(lambda a,b: a if a>b else b, array)
+
+print(maximum([1,4,3,2,3,-1]))
+
+
+
